43fd0d93ebaffc48e7be4dd43292d6fdf0fdd945
agodf <@haochen.yang>
2022-04-26 00:05:18 +0800

    update

--- config/config.ts
+++ config/config.ts
@@ -3,6 +3,8 @@ import menus from './menus';
 import { StyleImportBabel } from '../src/utils/plugin';
 
 export default defineConfig({
+  base: 'lacus',
+  publicPath: 'lacus',
   title: 'lacus',
   favicon: '/favicon.svg',
   logo: '/logo.png',
